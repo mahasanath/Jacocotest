1f2b9bea7f4a8eea3fe740ee50f893faddc7878f
max-pi <maksym.p@gmail.com>
2013-05-07 13:03:42 -0400

    added FIZZBUZZ output and FIZZ and BUZZ fields to FizzBuzz.java

--- src/main/java/ca/on/oicr/FizzBuzz.java
+++ src/main/java/ca/on/oicr/FizzBuzz.java
@@ -1,13 +1,22 @@
 package ca.on.oicr;
 
 public class FizzBuzz {
+	
+	public static String FIZZ = "FIZZ";
+	public static String BUZZ = "BUZZ";
 
 	public String decide(int i) {
 		if (i == 3) {
-			return "FIZZ";
+			return FIZZ;
 		}
 		else if (i == 5) {
-			return "BUZZ";
+			return BUZZ;
+		}
+		else if (i == 8) {
+			return "";
+		}
+		else if (i == 15) {
+			return FIZZ+BUZZ;
 		}
 		return "";
 	}
