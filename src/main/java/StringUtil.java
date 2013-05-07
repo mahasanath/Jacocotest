ede10a9cbb98a169b369955983d680af7c80ebde
Tony DeBat <tony.debat@gmail.com>
2013-05-07 12:45:45 -0400

    Added comment.

--- src/main/java/StringUtil.java
+++ src/main/java/StringUtil.java
@@ -1,5 +1,6 @@
 public class StringUtil {
 
+   // Fix and simplify code.
    public static boolean isNullOrEmpty(String s) {
       if (s == null && s.isEmpty()) { return true; }
       return false;
