487c519b0501a1987701ad75f0b90954649f02cd
max-pi <maksym.p@gmail.com>
2013-05-07 11:58:46 -0400

    added BUZZ output to FizzBuzz.java

--- src/main/java/ca/on/oicr/FizzBuzz.java
+++ src/main/java/ca/on/oicr/FizzBuzz.java
@@ -3,8 +3,12 @@ package ca.on.oicr;
 public class FizzBuzz {
 
 	public String decide(int i) {
-		// TODO Auto-generated method stub
-		return "FIZZ";
+		if (i == 3) {
+			return "FIZZ";
+		}
+		else {
+			return "BUZZ";
+		}
 	}
 
 }
