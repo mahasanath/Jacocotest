d18423d1d11f7c754d18762787952e40faf83261
Tony DeBat <tony.debat@gmail.com>
2013-05-07 11:49:06 -0400

    Added test to ensure 5 results in BUZZ.

--- src/test/java/ca/on/oicr/FizzBuzzTest.java
+++ src/test/java/ca/on/oicr/FizzBuzzTest.java
@@ -14,11 +14,14 @@ public class FizzBuzzTest {
       sut = new FizzBuzz();
    }
 
-
-
    @Test
-   public void test() {
+   public void test_with_3() {
       assertEquals("FIZZ", sut.decide(3));
+
    }
 
+   @Test
+   public void test_with_5() {
+      assertEquals("BUZZ", sut.decide(5));
+   }
 }
