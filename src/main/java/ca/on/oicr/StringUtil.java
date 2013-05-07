7e8d899171dfd86ab7db97a0a961be366ad45e7c
max-pi <maksym.p@gmail.com>
2013-05-07 14:37:43 -0400

    added StringUtilTest, fixed StringUtil, added emma plugin to pom

--- src/main/java/ca/on/oicr/StringUtil.java
+++ src/main/java/ca/on/oicr/StringUtil.java
@@ -1,9 +1,11 @@
+package ca.on.oicr;
+
 public class StringUtil {
 
    // Create unit tests in StringUtilTest to "figure" out what's wrong.
    // Simplify code.
    public static boolean isNullOrEmpty(String s) {
-      if (s == null && s.isEmpty()) { return true; }
+      if (s == null || s.isEmpty()) { return true; }
       return false;
    }
 }
