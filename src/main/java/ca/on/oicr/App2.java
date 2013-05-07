e259eb8415204813949093ff51d25ecaedc0450f
max-pi <maksym.p@gmail.com>
2013-05-07 11:00:11 -0400

    changed App2.java

--- src/main/java/ca/on/oicr/App2.java
+++ src/main/java/ca/on/oicr/App2.java
@@ -4,10 +4,6 @@ package ca.on.oicr;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App2 {
+   
 }
